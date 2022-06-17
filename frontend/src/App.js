c815455862731b09875b7803f23d09f3882c1bee
Prasiddha777 <be2018se630@gces.edu.np>
2022-06-17 11:53:41 +0545

    lisitng

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,30 @@
+import data from "./data";
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">Style&#38;Wear</a>
       </header>
-      <main>list Products</main>
+      <main>
+        <h1>Products</h1>
+        <div className="products">
+          {data.products.map((products) => (
+            <div className="product" key={products.slug}>
+              <a href={`/product/${products.slug}`}>
+                <img src={products.image} alt={products.name} />
+              </a>
+              <a href={`/product/${products.slug}`}>
+                <p>{products.name}</p>
+              </a>
+              <p>
+                <strong>Rs.{products.price}</strong>
+              </p>
+              <button>Add to Cart</button>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
