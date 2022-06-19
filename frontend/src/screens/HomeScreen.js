d259d2a048baa21fc0a9ba0fbea86b81d7b2029b
Prasiddha777 <be2018se630@gces.edu.np>
2022-06-19 15:52:04 +0545

    fetch product from backend

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,12 +1,27 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import data from "../data";
+// import data from "../data";
+import axios from "axios";
 
 function HomeScreen() {
+  // Array having products and setproducts
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    //call api and get products from backend
+    const fetchData = async () => {
+      //call axios.get method to send an ajax request from api/product address and put in variable (result)
+      const result = await axios.get("/api/products");
+      //call setproducts function and as a parameter return result.data;
+      setProducts(result.data);
+    };
+    //
+    fetchData();
+  }, []);
   return (
     <div>
       <h1>Products</h1>
       <div className="products">
-        {data.products.map((products) => (
+        {products.map((products) => (
           <div className="product" key={products.slug}>
             <Link to={`/product/${products.slug}`}>
               <img src={products.image} alt={products.name} />
