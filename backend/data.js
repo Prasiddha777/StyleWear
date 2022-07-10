0357a2be5b52b7aa1062c2629a0fe8c3c1226441
Prasiddha777 <be2018se630@gces.edu.np>
2022-07-10 07:38:27 +0545

    seed user

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from "bcryptjs";
+
 const data = {
+  users: [
+    {
+      name: "Prasiddha",
+      email: "admin@example.com",
+      password: bcrypt.hashSync("123456"),
+      isAdmin: true,
+    },
+    {
+      name: "Sakchham",
+      email: "user@example.com",
+      password: bcrypt.hashSync("123456"),
+      isAdmin: false,
+    },
+  ],
   products: [
     // Object=>{}
     {
